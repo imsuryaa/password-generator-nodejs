77ac75f8f33b708998b602ebafce8cec9b4d1aab
suryatejaj97 <suryatejaj97@gmail.com>
2021-07-08 19:38:51 +0530

    initial code setup

---
+++ index.js
@@ -0,0 +1,12 @@
+const program = require("commander");
+
+program.version("1.0.0").description("Password Generator");
+
+program
+  .option("-l, --length <number>", "length of password", "8")
+  .option("-s, --save", "save password to passwords.txt")
+  .option("-nn, --no-numbers", "remove numbers")
+  .option("-ns, --no-symbols", "remove symbols")
+  .parse();
+
+console.log(program.opts());
